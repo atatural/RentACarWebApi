abc881807a81881ef108b998427d65fca0e537b2
atatural <ataberktural@galatasaray.net>
2024-06-27 23:03:04 +0300

    Add project files.

---
+++ Business/Abstracts/IBrandService.cs
@@ -0,0 +1,17 @@
+﻿using Entities.Abstracts;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Business.Abstracts
+{
+    public interface IBrandService
+    {
+        //Brand Add(Brand brand);
+        CreatedBrandResponse Add(CreateBrandRequest brand);
+        
+        List<Brand> GetAll();
+    }
+}
